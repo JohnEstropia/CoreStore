25bc4a189f4fe21367fd8179d09455484b660a0c
John Rommel Estropia <john.rommel.estropia@gmail.com>
2014-09-14 21:51:40 +0900

    Initial Commit

---
+++ HardcoreData/HardcoreData.h
@@ -0,0 +1,19 @@
+//
+//  HardcoreData.h
+//  HardcoreData
+//
+//  Created by John Rommel Estropia on 2014/09/14.
+//  Copyright (c) 2014 John Rommel Estropia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HardcoreData.
+FOUNDATION_EXPORT double HardcoreDataVersionNumber;
+
+//! Project version string for HardcoreData.
+FOUNDATION_EXPORT const unsigned char HardcoreDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HardcoreData/PublicHeader.h>
+
+
