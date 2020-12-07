668b5ad606847ba1fef1f11d6ac951a42cfd7ba8
John Estropia <rommel.estropia@gmail.com>
2020-12-07 14:08:05 +0900

    Update playground samples to use Field properties instead of old syntax

--- Playground_macOS.playground/Contents.swift
+++ Playground_macOS.playground/Contents.swift
@@ -6,14 +6,24 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 /// Model Declaration =====
 class Animal: CoreStoreObject {
-    let species = Value.Required<String>("species", initial: "Swift")
-    let master = Relationship.ToOne<Person>("master")
-    let color = Transformable.Optional<NSColor>("color", initial: .orange)
+
+    @Field.Stored("species")
+    var species: String = "Swift"
+
+    @Field.Coded("color", coder: FieldCoders.NSCoding.self)
+    var color: NSColor = .orange
+
+    @Field.Relationship("master")
+    var master: Person?
 }
 
 class Person: CoreStoreObject {
-    let name = Value.Optional<String>("name")
-    let pets = Relationship.ToManyUnordered<Animal>("pets", inverse: { $0.master })
+
+    @Field.Stored("name")
+    var name: String?
+
+    @Field.Relationship("pets", inverse: \.$master)
+    var pets: Set<Animal>
 }
 /// =======================
 
@@ -24,6 +34,10 @@ let dataStack = DataStack(
         entities: [
             Entity<Animal>("Animal"),
             Entity<Person>("Person")
+        ],
+        versionLock: [
+            "Animal": [0x4a201cc685d53c0a, 0x16e6c3b561577875, 0xb032e2da61c792a0, 0xa133b801051acee4],
+            "Person": [0xca938eea1af4bd56, 0xbca30994506356ad, 0x7a7cc655898816ef, 0x1a4551ffedc9b214]
         ]
     )
 )
@@ -42,12 +56,12 @@ dataStack.addStorage(
                 asynchronous: { transaction in
 
                     let animal = transaction.create(Into<Animal>())
-                    animal.species .= "Sparrow"
-                    animal.color .= .yellow
+                    animal.species = "Sparrow"
+                    animal.color = .yellow
 
                     let person = transaction.create(Into<Person>())
-                    person.name .= "John"
-                    person.pets.value.insert(animal)
+                    person.name = "John"
+                    person.pets.insert(animal)
                 },
                 completion: { result in
 
@@ -58,13 +72,16 @@ dataStack.addStorage(
 
                     case .success:
                         /// Accessing Objects =====
-                        let bird = try! dataStack.fetchOne(From<Animal>().where(\.species == "Sparrow"))!
-                        print(bird.species.value)
-                        print(bird.color.value as Any)
+                        let bird = try! dataStack.fetchOne(
+                            From<Animal>()
+                                .where(\.$species == "Sparrow")
+                        )!
+                        print(bird.species)
+                        print(bird.color as Any)
                         print(bird)
 
-                        let owner = bird.master.value!
-                        print(owner.name.value as Any)
+                        let owner = bird.master!
+                        print(owner.name as Any)
                         print(owner.pets.count)
                         print(owner)
                         /// =======================
